9fea32f0d6e03ce34159102293536bc862ab7724
jbgzr <74554352+jbgzr@users.noreply.github.com>
2020-11-18 14:54:23 +0100

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQD2og/quO8NrS+qkTJEILTKU3oup20lhXCqDCUiKWpK93kKtS9ONwZGbQxEZsUfrGagSQtHJfUb6YduBki498XgSCD3Z33SuL2M9Ays0B3iATTwtiOi3ZMa7psNag4vlHrpIx+CtzjW2a3C7FNrFU1kpuTFPoD1a6hJOpnqTouKPTTC46/Xf4N9rG+AGmSwAlRqgDOE7B7mMf+3exDu4mdSi0IuckiNop+1YLW+zJtBSo/fhCPkH467Gt5nw37hSZgDTXq9G7RNVVBk1roa4Rrm9WuxJzrCedIQoUFNaKJwmwcPDqQNwra0BnhiCmVGQhPmHdAIA9rECwBDaiNs3JaF',
 	}  
 }
