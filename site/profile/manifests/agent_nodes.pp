ee0b68a06593041e1577b95e8f288d1b1e0b1b19
jbgzr <74554352+jbgzr@users.noreply.github.com>
2020-11-18 14:58:51 +0100

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -7,11 +7,11 @@ class profile::agent_nodes {
     ip      => '172.18.0.2'
   }
   host {'web.puppet.vm':
-    ensure  => present
-    ip      => '172.18.0.2'
+    ensure  => present,
+    ip      => '172.18.0.2',
   }
   host {'db.puppet.vm':
-    ensure  => present
-    ip      => '172.18.0.3'
+    ensure  => present,
+    ip      => '172.18.0.3',
   }
 }
