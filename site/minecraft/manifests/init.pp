3054c504af1abd45a7101aa58027df9e7a8e6b11
jbgzr <74554352+jbgzr@users.noreply.github.com>
2020-11-23 19:47:47 +0100

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -4,12 +4,12 @@ class minecraft {
   }
   file {'/opt/minecraft/server.jar':
     ensure => file,
-    source => https://launcher.mojang.com/v1/objects/35139deedbd5182953cf1caa23835da59ca3d7cd/server.jar,
+    source => 'https://launcher.mojang.com/v1/objects/35139deedbd5182953cf1caa23835da59ca3d7cd/server.jar',
   }
   package {'java':
     ensure => present,
   }
-  file {'opt/minecraft/eula.txt':
+  file {'/opt/minecraft/eula.txt':
     ensure => file,
     content => 'eula=true',
   }
