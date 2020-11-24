bb3ddaa7516ddb34d7ede48138f569009e450cac
jbgzr <74554352+jbgzr@users.noreply.github.com>
2020-11-24 11:04:18 +0100

    Update base.pp

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -2,11 +2,5 @@ class profile::base {
   user {'admin':
     ensure => present,    
   }
-  user { 'vagrant':
-    ensure   => present,
-  }
-  file { '/home/vagrant':
-    ensure => 'directory',
-  }
   include profile::ssh_server
 }
