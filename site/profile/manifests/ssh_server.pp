1510b1232c1c002363c4ae579baa3fefbb67e956
jbgzr <74554352+jbgzr@users.noreply.github.com>
2020-11-24 11:02:52 +0100

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -12,12 +12,4 @@ class profile::ssh_server {
 		type   => 'ssh-rsa',
 		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDNjk5102Cq3ZlQVuUIwkNg/O4HLtNVatxqxhMLljOVkSIsIsmpvONoIaat+xycCE2gqw/Wl3dqgrNsIomGqSpfOT+bIsUcAtPF4x02if2DJeKEOOIJ2DOES+rgWjzjJnHsufPqV4DEK146vHyrpXfCkwwmqpiQeW8nCdR68XETWgJwqs7d4u6gI0FmUBWQKFvF6xXdAWITLHaK2hdTSkVOziM5aopJyzCJS9Kn6w0cksxmazJErHwvJC8c1ooAWpL7jYVgKz9AE+cilX9drtNbk3xwGQTzmWIYYl2pp3lMgVgVfNvcb7XtBrCDq4f54GmY8nIE0vegALiVLe6l6jk5',
 	}  
-	ssh_authorized_key { 'vagrant@master.puppet.vm':
-		ensure => present,
-		user   => 'vagrant',
-		type   => 'ssh-rsa',
-		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQD1UVN5pHopkbnVkuz6AO02sM1ssY3PTi2OzfEmElyJDq0F+wJvQaM9uDE2vWYuYKmbj8ALdeXDVFTN3QfR4oWJvHOnGoG16n7kaInML6+l1+kyxjT251euW+e9p16TameSCFMWR7UItYMPuNM04vpIzhUzshes0SZkkzaHhLUUMqxSk+vQoHxO4p3NaWwm6QL2e8cJTSq6fIGSRLgLLfci9nhE0F+x0xNuTbA5uMsajev/OQTZOX/o8BKPuP4pTx3cc2eQoJqVwDRLQ1mEdrc5HS0MP4NWtWYCM5IsrLwLB44VboH0sBS/UhXoDad9s4zu8wODBqX5+ltrPuDHoZ7D',
-	} 
-	
-	
 }
